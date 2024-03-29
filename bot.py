f16a0e9842fb0ee3502daa8c992059f3a1f94ca0
SujithPS <sujithps534@gmail.com>
2024-03-29 19:12:00 +0530

    Update welcome message  - use the channel name inside the message

--- bot.py
+++ bot.py
@@ -20,11 +20,11 @@ class DisboBot(commands.Bot):
         print(f'Message from {message.author}: {message.content}')
 
     async def on_member_join(self, member):
-        test_channel = discord.utils.get(member.guild.channels, name=os.getenv("DEVELOPER_CHANNEL"))
+        developer_channel = discord.utils.get(member.guild.channels, name=os.getenv("DEVELOPER_CHANNEL"))
 
-        if test_channel:
-            await test_channel.send(f'Welcome {member.mention} to the developer channel!')
-            await test_channel.send(f"{os.getenv('FIRST_CONTRIBUTION_MESSAGE')}")
+        if developer_channel:
+            await developer_channel.send(f'Welcome {member.mention} to the \'{os.getenv("DEVELOPER_CHANNEL")}\' channel!')
+            await developer_channel.send(f"{os.getenv('FIRST_CONTRIBUTION_MESSAGE')}")
 
         else:
             print("Test channel not found!")
